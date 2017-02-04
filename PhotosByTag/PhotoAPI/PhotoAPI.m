e2a8e5daf62fd93aeba030ed94d44b989cd66c0c
Ayhan Dorman <dorman.ayhan@gmail.com>
2017-02-05 00:07:48 +0300

    Added gzip support to service connection.

--- PhotosByTag/PhotoAPI/PhotoAPI.m
+++ PhotosByTag/PhotoAPI/PhotoAPI.m
@@ -38,9 +38,9 @@ NSURLSessionDataTask *dataTask;
     
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
     
-    [request setTimeoutInterval:60.0]; 
+    [request setTimeoutInterval:60.0];
     
-    [request addValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"];
+    [request addValue:@"gzip" forHTTPHeaderField:@"Accept-Encoding"]; 
     
     [request setHTTPMethod:@"GET"];
     
