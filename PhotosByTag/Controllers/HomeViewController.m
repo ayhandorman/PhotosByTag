3f64520c7a719ed1b622b9f93a6276eeb5c74dde
Ayhan Dorman <dorman.ayhan@gmail.com>
2017-02-04 19:09:53 +0300

    Moved HomeViewController to Controllers folder.

---
+++ PhotosByTag/Controllers/HomeViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  PhotosByTag
+//
+//  Created by Ayhan Dorman on 04/02/2017.
+//  Copyright © 2017 Modanisa. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@end
+
+@implementation HomeViewController
+
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    
+    return UIStatusBarStyleLightContent;
+    
+}
+
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
